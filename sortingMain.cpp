e85d1ab78e4dcd022f1ffc60052634ba2ae502a3
gbonn <gbonn@10.0.0.125>
2020-05-19 16:56:25 -0400

    Im impressed at how far we've gotten and I love and appreciate both of you.cpp

--- sortingMain.cpp
+++ sortingMain.cpp
@@ -6,6 +6,10 @@
  */
 
 #include "sortingFile.hpp"
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
 
 int main(){
 
@@ -14,11 +18,27 @@ int main(){
 	//Testing Part A
 	int *testArr = MakeArray(100);
 	//selectionSort(testArr, 100, 100);
-	//mergeSort(testArr,0,99);
-	quickSort(0, 100,testArr);
+	mergeSort(testArr,0,99);
+	//quickSort(0, 99,testArr);
+	//insertionSort(testArr, 100);
 	PrintArray(testArr,100);
 	delete testArr;
 
+	//Testing Part B
+	int *testArr2 = MakeArray(10000);
+	auto start = high_resolution_clock::now();
+
+	//selectionSort(testArr, 100, 100);
+
+	mergeSort(testArr2, 0, 9999);
+	//quickSort(0, 99,testArr);
+	//insertionSort(testArr, 100);
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	int ms = duration.count();
+	//PrintArray(testArr, 100);
+	cout << "Time for insertionSort is " << ms <<endl;
+	delete testArr2;
 
 
 
