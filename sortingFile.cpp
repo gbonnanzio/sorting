9127e2cb6820f605774211109ec3c67a0f77b945
chloe <chloe@192.168.123.221>
2020-05-19 16:06:42 -0400

    makeArray

--- sortingFile.cpp
+++ sortingFile.cpp
@@ -129,3 +129,13 @@ void merge(int arr[], int le, int m, int r){
 
 
 
+
+
+int *MakeArray(int arr[], int size){
+	int * array = new int[size];
+	for (int i=0;i<size;i++){
+		array[i]=rand();
+	}
+	return array;
+}
+
